c01a12f1d298ecc3e562e5236a8ee85a0118a7c3
ThomasDoue <thomasdoue49100@gmail.com>
2022-08-26 11:50:46 +0200

    Filtre Ville casiment fini (problème FilterVilleType does not exist)

---
+++ src/Repository/FilterVilleRepository.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Ville;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Security\Core\Exception\UnsupportedUserException;
+use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
+
+class FilterVilleRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Ville::class);
+    }
+
+
+    public function VilleFilter(string $text){
+        $entityManager = $this->getEntityManager();
+
+        $dql1 = "SELECT u FROM App\Entity\Ville u
+               WHERE u.nom LIKE :text ";
+        $query= $entityManager->createQuery($dql1)->setParameter('text', '%'.$text.'%');
+        return $query->getResult();
+}}
