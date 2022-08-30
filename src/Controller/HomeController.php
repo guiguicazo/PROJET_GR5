fe0f39a481e9e32513f2b24ecb693e6d9f329088
Renan-ENI <renan.guitet2022@campus-eni.fr>
2022-08-30 16:33:14 +0200

    Merge remote-tracking branch 'origin/main'

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -270,12 +270,14 @@ class HomeController extends AbstractController
     }
 
     #[Route('/inscrireSortie/{id_sortie}', name: 'app_sortie_inscrire', methods: ['GET'])]
-    public function inscrire($id_sortie,DateRepository $dateRepository): Response
+    public function inscrire($id_sortie,DateRepository $dateRepository,EntityManagerInterface $entityManager): Response
     {
         $sortie = $dateRepository->find($id_sortie);
         $user = $this->getUser();
         $sortie->setNbInscrit(1);
         $sortie->addParticipant($user);
+        $entityManager->persist($sortie);
+        $entityManager->flush();
                 return $this->redirectToRoute('app_recapAll');
     }
 
